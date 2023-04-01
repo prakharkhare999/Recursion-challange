1ee966857220649397d1fe41736ae0c34a290ffd
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2023-04-01 21:49:56 +0530

    Create Find keys in string.cpp

---
+++ Find keys in string.cpp
@@ -0,0 +1,25 @@
+// Day(3) ---- Findkeys in string
+#include <iostream>
+
+using namespace std;
+bool findchar(string str,int n,int i, char key)
+{
+    if(i>=n){
+        return false;
+    }
+    if(str[i]==key){
+        return true;
+        
+    }
+    return findchar(str,n,i+1,key);
+   
+}
+int main(){
+    string str="prakharkhare";
+  int n=str.length();
+  int i=0;
+ char key='z';
+  bool ans=findchar(str,n,i,key);
+  cout<<ans;
+
+}
