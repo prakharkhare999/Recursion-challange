a57a9c3c93f39da2d8b23db19bacfe85d8f0522a
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2023-04-03 18:28:46 +0530

    Update Subsequence of string.cpp

--- Subsequence of string.cpp
+++ Subsequence of string.cpp
@@ -1,1 +1,30 @@
-// Day(4) 
+// Day(4)  subsequences of string
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+void subsequences(string str,string output,int i){
+    if(i>=str.length()){
+    cout<<output<<endl;    
+    return ;
+    }
+    
+    //exclude
+    subsequences(str,output,i+1);
+    
+    //include
+  output.push_back(str[i]);
+    
+    subsequences(str,output,i+1);
+    
+}
+int main() {
+    
+    string str="abc";
+    string output="";
+    int i=0;
+    subsequences(str,output,i);
+    
+    
+
+    return 0;
+}
